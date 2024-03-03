726e83e28b4ed89b547dbe47c4a95e58b8b144ce
lolcatproductions <66800621+lolcatproductions@users.noreply.github.com>
2024-03-03 22:50:56 +0100

    feat: request data from url and format into dict

--- meallistgen.py
+++ meallistgen.py
@@ -1,7 +1,39 @@
 import requests
+import csv
 
-url = "https://pastebin.com/raw/sKNP6999"
+url = "http://pastebin.com/raw/sKNP6999"
 
-data = requests.get(url)
 
-print(data.text)
+def format_csv_data(dirty_data: list) -> dict[str: int]:
+    """makes dirty csv data into a dictionary with each key: value pair being 'name of item': weight"""
+    for i, row in enumerate(dirty_data):
+        if len(row) < 2:
+            # append "1" as weight to row if row doesn't have weight value
+            row.append("1")
+        
+        row[1] = int(row[1].strip())  # strip whitespaces from weight and convert to int
+        dirty_data[i] = tuple(row)
+    
+    return {
+        item: weight
+        for item, weight
+        in dirty_data
+    }
+    
+
+def get_csv_data(url) -> list[list[str]]:
+    request = requests.get(url)
+
+    if (status := request.status_code) != 200:
+        print(f"error: unexpected status code from response (expected 200, got {status})")
+        exit(1)
+
+    if not (content_type := request.headers["Content-Type"]).startswith("text/plain"):
+        print(f"error: Content-Type is not plain text (got {content_type})")
+        exit(1)
+
+    return list(csv.reader(request.text.splitlines()))
+
+
+data = format_csv_data(get_csv_data(url))
+print(data)
